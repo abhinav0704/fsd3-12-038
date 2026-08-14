b8499e165f9836f9bffa304be49be6cd01821956
Abhinav <yadavabhinav0704@gmail.com>
2026-08-14 15:16:07 +0530

    crud

---
+++ lab-2/crud.js
@@ -0,0 +1,62 @@
+import readline from "readline/promises";
+import { stdin, stdout } from "process";
+import { readFile, writeFile } from "fs/promises";
+
+const FILE = "products.json";
+
+const getCart = async () => {
+  const data = await readFile(FILE, "utf-8");
+  return JSON.parse(data);
+};
+
+const saveCart = async (myCart) => {
+  await writeFile(FILE, JSON.stringify(myCart, null, 2));
+};
+const addTocart = async (product) => {
+  const myCart = await getCart();
+  const isFound = myCart.find((item) => item.id === product.id);
+  if (isFound) {
+    isFound.qty += product.qty;
+  } else {
+    myCart.push(product);
+  }
+  await saveCart(myCart);
+  console.log(`Product added with id ${product.id} into cart`);
+};
+
+const main = async () => {
+  let choice;
+  const cin = readline.createInterface({ input: stdin, output: stdout });
+  do {
+    console.log("Welcome to Flipkart 🛒🤷‍♂️😁");
+    console.log("1.................Show Cart");
+    console.log("2.................Add to Product");
+    console.log("3.................Remove Product");
+    console.log("4.................Update Quantity");
+    console.log("5.................Checkout");
+    choice = await cin.question("Enter your choice: ");
+    switch (Number(choice)) {
+      case 1:
+        console.log("Show products");
+        break;
+      case 2:
+        console.log("Product added");
+        break;
+      case 3:
+        console.log("remove product");
+        break;
+      case 4:
+        console.log("Update product quantity");
+        break;
+      case 5:
+        console.log("See you later");
+        break;
+      default:
+        console.log("Invalid choice! try again😒");
+        break;
+    }
+  } while (choice != 5);
+  cin.close();
+};
+
+main();
--- lab-2/readme.md
+++ lab-2/readme.md
@@ -7,3 +7,19 @@ Js is synchronous and single threaded bydefault
 - with BrowserAPI- setTimeout,setInterval,setIntermediate,nexTrick
 - with promises
 - with event handlers
+## CRUD Project 
+assume we are making a cart related project 
+- user can add any product(id,name,price,qty) into cart 
+- user can see all the items of cart 
+- user can remove item from cart
+- user can also update quantity of product 
+- all the items should be stored after termination of project
+CRUD
+C-CREATE
+R-RETRIVE
+U-UPDATE
+D-DELETE
+CRUD IS IN ME SO I TAKE IT VERY EASILY IF 
+YOU WANT TO ME TO DO TGAT THEN I CAN DO WHAT YOU WANT 
+HELLO MY NAME IS ABHINAV 
+I'M 20 YEAR OLD 
