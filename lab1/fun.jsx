c7b21b50f2db79d4dd76adc94ea9d2103d311e13
Abhinav <yadavabhinav0704@gmail.com>
2026-09-18 14:38:38 +0530

    ab

--- lab1/fun.jsx
+++ lab1/fun.jsx
@@ -16,12 +16,7 @@ function digitToWord(digit) {
       return "Six";
     case 7:
       return "Seven";
-    case 8:
-      return "Eight";
-    case 9:
-      return "Nine";
-    default:
-      return "Invalid digit";
+
   }
 }
 
